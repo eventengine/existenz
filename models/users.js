67c7cb1e19fa06ad88ae72fa0aae9f32e8d08f5f
jpbeaudet <jpbeaudet84@gmail.com>
2016-03-22 16:37:31 -0400

    added users collections on creation

--- models/users.js
+++ models/users.js
@@ -4,13 +4,13 @@ var Schema = mongoose.Schema;
 
 var Users = new Schema({
 	meta:{ 
-		last_modified: Date,
+		last_modified: { type: Date, default: Date.now },
 		username: String,
 		password: String,
 		hdwallet: String
 	},
 	profile:{
-		last_modified: Date,
+		last_modified: { type: Date, default: Date.now },
 		avatar_url: String,
 		email: String,
 		description: String	 
--- routes/routes.js
+++ routes/routes.js
@@ -10,14 +10,13 @@ var api = require('../routes/api');
 var express = require('express');
 var passport = require('passport');
 var Account = require('../models/account');
-
-
+var Users = require('../models/users');
+var coluapi   = require('../scripts/coluApi-client.js'); // get our colu-api method library
 module.exports = function (app) {
 	
 	//Main routes
 	///////////////////////////
 	app.get('/', index.index);
-	
 	
 	// Colu-Api Routes
 	/////////////////////////////
@@ -25,6 +24,7 @@ module.exports = function (app) {
 	app.get('/getAdress', api.getAdress);
 	app.get('/issueAsset', api.issueAsset);
 	app.get('/sendAsset', api.sendAsset);
+	
 	// auth routes
 	/////////////////////////////
 	app.get('/login', function(req, res) {
@@ -44,8 +44,34 @@ module.exports = function (app) {
                if (err) {
                     return next(err);
                 }
-
-                res.redirect('/');
+               // if registering is a success create a user document in the users collection
+               // and attribute a hdwallet address to the user
+               var _cb = function(err, response){
+                   var user = new Users({
+               		meta:{ 
+              				username: req.body.username,
+              				password: req.body.password,
+              				hdwallet: response.result // get a hdwallet adress
+              				},
+              			profile:{
+              				avatar_url: "",
+              				email: "",
+              				description: ""	 
+              					}
+                  });
+      				user.save(function(err,_id) {
+      					if (err) throw err;
+      					console.log("User was created. Name: "+ req.body.username+ " Hdwallet: "+response.result);
+      				});
+            	   res.redirect('/'); 
+               };
+           		var json_data = {
+        		    jsonrpc: "2.0", // mandatory
+        		    method: "hdwallet.getAddress",
+        		    // mandatory
+        		    id: "1" // mandatory if response is needed
+        		};
+        		coluapi.postToApi('', json_data, _cb);  
            });
         });
    });
@@ -69,15 +95,15 @@ module.exports = function (app) {
         res.redirect('/');
     });
     });	
-
+	
+	// error handlers
+	/////////////////////////////////
 	// catch 404 and forward to error handler
 	app.use(function(req, res, next) {
 	    var err = new Error('Not Found');
 	    err.status = 404;
 	    next(err);
 	});
-	// error handlers
-	/////////////////////////////////
 	
 	// development error handler
 	// will print stacktrace
